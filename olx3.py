e30d9280d797e8c827ec68cf891a1a73b543d884
tuliotrefzger <tuliotrefzgermello@gmail.com>
2024-04-28 22:49:27 -0300

    Working v1
    Found a way to retrieve all ads from a source and message the sellers who are adequate to
    the needs provided

--- olx3.py
+++ olx3.py
@@ -2,8 +2,12 @@ import time
 import random
 import pprint
 import datetime
-import pandas as pd
+
+# import pandas as pd
+import pyautogui
 import undetected_chromedriver as uc
+import pyscreeze
+
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,6 +15,9 @@ from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+
+pyscreeze.USE_IMAGE_NOT_FOUND_EXCEPTION = False
 
 
 def get_driver():
@@ -24,9 +31,9 @@ def get_driver():
 
 def wait_based_on_iteration(iteration):
     if iteration % 16 == 0:
-        wait_random_time(120, 180)
+        wait_random_time(60, 80)
     elif iteration % 25 == 0:
-        wait_random_time(180, 210)
+        wait_random_time(90, 110)
     else:
         wait_random_time(10, 20)
 
@@ -156,23 +163,51 @@ def get_ad_publish_date(ad_page, url):
     return None
 
 
-def get_cars(driver, search_infos):
-    driver.get(
-        f"https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/{search_infos['brand']}/{search_infos['model']}/estado-df?f=p&me={search_infos['maxKm']}&ms={search_infos['minKm']}&re={search_infos['maxYear']}&rs={search_infos['minYear']}"
+def currency_to_number(currency_string):
+    """
+    This function takes a string representing a currency value and removes the currency symbol and formatting to return the integer value.
+
+    Args:
+        currency_string: A string representation of a currency value. (e.g., "R$ 100.000")
+
+    Returns:
+        The integer value of the currency string without decimals. (e.g., 100000)
+    """
+    # Remove all characters except numbers, ",", "+" or "-".
+    numbers = "".join(
+        char for char in currency_string if char.isdigit() or char in ",+-"
     )
-    # time.sleep(1200)
+    # Split the string at the decimal point (if it exists)
+    split_string = numbers.split(",")
+    # Use only the integer part (index 0) and convert to integer
+    return int(split_string[0])
+
+
+def login(driver):
+    driver.get("https://www.olx.com.br")
+    # Login part
     try:
         WebDriverWait(driver, 30).until(
             EC.element_to_be_clickable(
                 (
                     By.CSS_SELECTOR,
-                    "#main-content > div.sc-f431cc3e-2.bBwRRe",
+                    "#header > nav > div > div.olx-header__column-right > ul > li.olx-header__profile-item > a",
                 )
             )
-        )
-    except TimeoutException:
-        print("TimeoutException: Element not found")
-        return None
+        ).click()
+        wait_random_time()
+        # time.sleep(120)
+        time.sleep(60)
+        driver.refresh()
+        wait_random_time()
+    except Exception as err:
+        print("Impossible to find login button")
+
+
+def get_cars(driver, search_infos):
+    driver.get(
+        f"https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/{search_infos['brand']}/{search_infos['model']}/estado-df?f=p&me={search_infos['maxKm']}&ms={search_infos['minKm']}&re={search_infos['maxYear']}&rs={search_infos['minYear']}"
+    )
 
     ad_urls = get_urls(driver)
     count = 0
@@ -194,17 +229,33 @@ def get_cars(driver, search_infos):
         car_price = get_car_price(ad_page, car_url)
         print(f"Price: {car_price}")
 
+        # Skips vehicle if price was not found
+        if not car_price:
+            continue
+
+        # Removes car more expensive than allowed
+        if currency_to_number(car_price) > search_infos["maxPrice"]:
+            print(f"Car {car_url} is more expensive than the maximum allowed price")
+            continue
+
+        fipe_price = get_fipe_price(ad_page, car_url)
+        print(f"Preço FIPE: {fipe_price}")
+
+        if fipe_price:
+            if currency_to_number(car_price) < 0.85 * currency_to_number(fipe_price):
+                print(
+                    "This vehicle is cheaper than 85% of its FIPE price, therefore we skipped it"
+                )
+                continue
+
+        average_olx_price = get_average_olx_price(ad_page, car_url)
+        print(f"Preço médio OLX: {average_olx_price}")
+
         ad_publishment_date = get_ad_publish_date(ad_page, car_url)
         print(f"Data de publicação: {ad_publishment_date}")
 
         user_since = get_user_since(ad_page, car_url)
         print(f"Usuário desde: {user_since}")
-
-        average_olx_price = get_average_olx_price(ad_page, car_url)
-        print(f"Preço médio OLX: {average_olx_price}")
-
-        fipe_price = get_fipe_price(ad_page, car_url)
-        print(f"Preço FIPE: ${fipe_price}")
 
         car_model = get_car_model(ad_page, car_url)
         print(f"Modelo: {car_model}")
@@ -218,14 +269,91 @@ def get_cars(driver, search_infos):
         car_km = get_car_km(ad_page, car_url)
         print(f"Kilometragem: {car_km}")
 
+        time.sleep(10)
+
+        try:
+            chat_button = "./images/chat-button-3.png"
+
+            button_location = pyautogui.locateOnScreen(
+                chat_button, confidence=0.8, grayscale=True
+            )
+
+            if button_location is not None:
+                # If the button is found, get its center coordinates
+                button_center = pyautogui.center(button_location)
+                print(button_center)
+                pyautogui.click(button_center[0], button_center[1])
+                print("Chat button found at:", button_center)
+            else:
+                print("Chat button not found on the screen.")
+                continue
+        except Exception as e:
+            print("Chat button not found on the screen.")
+            continue
+
+        # # -------------- Old find chat button ------------
+        # chat_button = "./chat-button-3.png"
+        #
+        # button_location = pyautogui.locateOnScreen(
+        #     chat_button, confidence=0.8, grayscale=True
+        # )
+        #
+        # if button_location is not None:
+        #     # If the button is found, get its center coordinates
+        #     button_center = pyautogui.center(button_location)
+        #     print(button_center)
+        #     pyautogui.click(button_center[0], button_center[1])
+        #     print("Chat button found at:", button_center)
+        # else:
+        #     print("Chat button not found on the screen.")
+        #     continue
+
+        # -------------- Send message part ------------
+        try:
+            input_field = WebDriverWait(driver, 30).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.CSS_SELECTOR,
+                        "#input-text-message",
+                    )
+                )
+            )
+
+            input_field.send_keys(search_infos["message"])
+            # # This part works and really sends a message. Leave commented while developing
+            # input_field.send_keys(Keys.ENTER)
+            wait_random_time()
+        except Exception as err:
+            print(f"Impossible to send message to client {car_url}")
+
         wait_based_on_iteration(count)
 
         print(f"Finished at: {datetime.datetime.now()}")
 
-        # if count >= 20:
-        #     break
 
-    # print(menu_page.prettify())
+def send_olx_message_automation(search_infos):
+    # create the driver object.
+    driver = get_driver()
+
+    login(driver)
+
+    # search_infos = {
+    #     "brand": "ford",
+    #     "model": "ka",
+    #     "minKm": 0,
+    #     "maxKm": 60000,
+    #     "minYear": 68,
+    #     "maxYear": 74,
+    #     "maxPrice": 200000,
+    #     "message": "Olá, tudo bem?",
+    # }
+
+    get_cars(driver, search_infos)
+
+    print("\nTHE END")
+
+    # close the driver.
+    driver.close()
 
 
 if __name__ == "__main__":
@@ -233,16 +361,22 @@ if __name__ == "__main__":
     # create the driver object.
     driver = get_driver()
 
+    login(driver)
+
     search_infos = {
-        "brand": "audi",
-        "model": "a3",
+        "brand": "ford",
+        "model": "ka",
         "minKm": 0,
-        "maxKm": 500000,
-        "minYear": 0,
+        "maxKm": 60000,
+        "minYear": 68,
         "maxYear": 74,
+        "maxPrice": 200000,
+        "message": "Olá, tudo bem?",
     }
 
     get_cars(driver, search_infos)
+
+    print("\nTHE END")
 
     # close the driver.
     driver.close()
